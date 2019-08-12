6fade3874c72522b3bd477439d43869429a9bb65
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-12 16:13:46 +0100

    fixed error with importing image and added movie box

---
+++ src/containers/MovieBox.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class MovieBox extends Component {
+
+  render() {
+    return(
+      <div className="movie-box">
+        <h2>moviebox!</h2>
+      </div>
+    )
+  }
+
+}
+
+export default CommentBox;
